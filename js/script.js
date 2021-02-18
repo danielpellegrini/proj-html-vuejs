929e83c99e4a64c5f3f4b13d1dd2fdc2aa8db3e0
Daniel <danielpellegrini87@gmail.com>
2021-02-18 17:59:44 +0100

    Section 1 draft completed

--- js/script.js
+++ js/script.js
@@ -1,9 +1,44 @@
-var app = new Vue({
- el: '#app',
- data: {
-   
+new Vue({
+  el: '#app',
+  data: {
+    sliderSection1: [
+        './img/section1/slider_slide2_img1.png',
+        './img/section1/slider_slide2_img2.png'
+    ],
+    imagesIndex: 0
+  },
 
- }
+  methods: {
+    next: function() {
+      if (this.imagesIndex >= this.sliderSection1.length - 1) {
+        this.imagesIndex = 0;
+      } else {
+        this.imagesIndex += 1;
+      }
+
+    },
+    previous: function() {
+      if (this.imagesIndex < 1) {
+        this.imagesIndex = this.sliderSection1.length - 1;
+      } else {
+        this.imagesIndex -= 1;
+      }
+    },
+
+    activeDot: function(index) {
+      if (index !== this.imagesIndex) {
+        return 'fas fa-circle'
+      } else {
+        return 'fas fa-circle active'
+      }
+    },
+
+    clickedDot: function(index) {
+      this.imagesIndex = index;
+    }
+
+  }
+
 });
 
 Vue.config.devtools = true;
