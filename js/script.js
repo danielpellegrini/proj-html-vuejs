c3e205f62c0ba4de1dbd613b46791db35789f550
Daniel <danielpellegrini87@gmail.com>
2021-02-23 11:52:36 +0100

    working on slider

--- js/script.js
+++ js/script.js
@@ -1,52 +1,80 @@
 new Vue({
   el: '#app',
   data: {
-    sliderSection1: [
 
-        // './img/section1/slider_slide3_img1.png',
-        // './img/section1/slider_slide3_img2.png',
-        // './img/section1/slider_slide3_img3.png',
-        './img/section1/slider_slide1_img2.png',
-        './img/section1/slider_slide1_img1.png',
-        // './img/section1/slider_slide2_img1.png',
-        // './img/section1/slider_slide2_img2.png',
+    "sliderIndex": 0,
+
+    "sliderSection1": [
+      {
+        "slider1": [{
+          "icons": [1, 2, 3, 4, 5, 6, 7, 8, 9],
+          "topText": 'Welcome to Fable!',
+          "middleText": 'Perfect Education',
+          "bottomText": 'For Your Child'
+        }],
+
+        "slider2": [{
+          "icons": [10, 11, 12, 13, 14],
+          "topText": 'Friendly Atmosphere',
+          "middleText": 'Welcoming Place',
+          "bottomText": 'For Every Child'
+        }],
+
+        "slider3": [{
+          "icons": [14, 15, 16, 17, 18],
+          "topText": 'Learning & Fun',
+          "middleText": 'Learning Program',
+          "bottomText": '& After-School Care'
+        }]
+
+      }
+
     ],
-    imagesIndex: 0,
-
 
   },
 
   methods: {
+
     next: function() {
-      if (this.imagesIndex >= this.sliderSection1.length - 1) {
-        this.imagesIndex = 0;
+
+      if (this.sliderIndex >= this.sliderSection1.length - 1) {
+        this.sliderIndex = 0;
       } else {
-        this.imagesIndex += 1;
+        this.sliderIndex += 1;
       }
 
+      return this.sliderIndex ++
     },
+
     previous: function() {
-      if (this.imagesIndex < 1) {
-        this.imagesIndex = this.sliderSection1.length - 1;
+      if (this.sliderIndex < 1) {
+        this.sliderIndex = this.sliderSection1.length - 1;
       } else {
-        this.imagesIndex -= 1;
+        this.sliderIndex -= 1;
       }
+
     },
 
     activeDot: function(index) {
-      if (index !== this.imagesIndex) {
-        return 'rectangle'
+      if (index !== this.sliderIndex) {
+        return 'rectangle-active'
       } else {
         return 'rectangle'
       }
     },
 
     clickedDot: function(index) {
-      this.imagesIndex = index;
-    }
+      this.sliderIndex = index;
+    },
 
-  }
+
+
+
+  },
+
+
 
 });
+
 
 Vue.config.devtools = true;
