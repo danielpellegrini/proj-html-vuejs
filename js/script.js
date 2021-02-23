b1f7d6065d4d6bf0accddbac89b867f1db9371fd
Daniel <danielpellegrini87@gmail.com>
2021-02-23 15:14:09 +0100

    dynamic background section 1 completed

--- js/script.js
+++ js/script.js
@@ -2,29 +2,34 @@ new Vue({
   el: '#app',
   data: {
 
-    "sliderIndex": 0,
+    sliderIndex: 0,
 
-    "sliderSection1": [
+    sliderSection1: [
       {
-        "slider1": [{
-          "icons": [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          "topText": 'Welcome to Fable!',
-          "middleText": 'Perfect Education',
-          "bottomText": 'For Your Child'
+        slider1: [{
+          iconsA: [1, 2, 3, 4],
+          iconsB: [5, 6, 7, 8, 9],
+          imageA: ['slide1_img1'],
+          imageB: ['slide1_img2'],
+          topText: 'Welcome to Fable!',
+          middleText: 'Perfect Education',
+          bottomText: 'For Your Child'
         }],
 
-        "slider2": [{
-          "icons": [10, 11, 12, 13, 14],
-          "topText": 'Friendly Atmosphere',
-          "middleText": 'Welcoming Place',
-          "bottomText": 'For Every Child'
+        slider2: [{
+          icons: [10, 11, 12, 13, 14],
+          images: ['slide2_img1', 'slide2_img2'],
+          topText: 'Friendly Atmosphere',
+          middleText: 'Welcoming Place',
+          bottomText: 'For Every Child'
         }],
 
-        "slider3": [{
-          "icons": [14, 15, 16, 17, 18],
-          "topText": 'Learning & Fun',
-          "middleText": 'Learning Program',
-          "bottomText": '& After-School Care'
+        slider3: [{
+          icons: [14, 15, 16, 17, 18],
+          images: ['slide3_img1', 'slide3_img2', 'slide3_img3'],
+          topText: 'Learning & Fun',
+          middleText: 'Learning Program',
+          bottomText: '& After-School Care'
         }]
 
       }
@@ -37,21 +42,22 @@ new Vue({
 
     next: function() {
 
-      if (this.sliderIndex >= this.sliderSection1.length - 1) {
-        this.sliderIndex = 0;
-      } else {
-        this.sliderIndex += 1;
-      }
 
-      return this.sliderIndex ++
+      // if (this.sliderIndex >= this.sliderSection1.length - 1) {
+      //   this.sliderIndex = 0;
+      // } else {
+      //   this.sliderIndex += 1;
+      // }
+      //
+      // return this.sliderIndex ++
     },
 
     previous: function() {
-      if (this.sliderIndex < 1) {
-        this.sliderIndex = this.sliderSection1.length - 1;
-      } else {
-        this.sliderIndex -= 1;
-      }
+      // if (this.sliderIndex < 1) {
+      //   this.sliderIndex = this.sliderSection1.length - 1;
+      // } else {
+      //   this.sliderIndex -= 1;
+      // }
 
     },
 
