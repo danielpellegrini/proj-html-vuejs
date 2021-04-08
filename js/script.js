aa9318b938d25062f845894ccdd867e0bb8a307c
Daniel <danielpellegrini87@gmail.com>
2021-04-08 09:33:58 +0200

    slide main backgrounds

--- js/script.js
+++ js/script.js
@@ -8,12 +8,10 @@ new Vue({
       open: false,
     },
     scrollState: 0, // Used to keep track of scroll position
-  
-    bg1: false,
-    bg2: true,
-    bg3: true,
 
-    currentImageIdx: 0,
+    bg0: false,
+    bg1: true,
+    bg2: true,
 
     sliderSection1: [
       {
@@ -21,17 +19,21 @@ new Vue({
         iconsB: [5, 6, 7, 8, 9],
         imageA: ['slide1_img1'],
         imageB: ['slide1_img2'],
+        toggle: 'visible'
       },
       {
         icons: [10, 14, 11, 12, 13],
         images: ['slide2_img1', 'slide2_img2'],
+        toggle: 'hidden'
       },
       {
         icons: [14, 15, 16, 17, 18],
         images: ['slide3_img1', 'slide3_img2', 'slide3_img3'],
+        toggle: 'hidden'
       }
     ],
 
+    currentImageIdx: 0,
     imagesList: [
       '_01-690x506',
       '_07-690x506',
@@ -43,8 +45,50 @@ new Vue({
   },
   methods: {
 
-    //  OLD VERSION SLIDER 
-    nextSlide: function () {
+
+    // MAIN BG SLIDER
+    toggleClass() {      
+      if (this.currentImageIdx === 0) {
+        this.bg0 = false;
+        this.bg1 = true;
+        this.bg2 = true;
+      };
+      if (this.currentImageIdx === 1 ) {
+        this.bg0 = true;
+        this.bg1 = false;
+        this.bg2 = true;
+      };
+      if (this.currentImageIdx === 2 ) {
+        this.bg0 = true;
+        this.bg1 = true;
+        this.bg2 = false;
+      };
+      // console.log(this.currentImageIdx);      
+    },
+
+    next() {
+      if (this.currentImageIdx >= this.sliderSection1.length - 1) {
+        this.currentImageIdx = 0;
+      } else {
+        this.currentImageIdx += 1;
+      }  
+      this.toggleClass();
+    },
+    prev(){
+      if (this.currentImageIdx < 1) {
+        this.currentImageIdx = this.sliderSection1.length - 1;
+      } else {
+        this.currentImageIdx -= 1;
+      }
+      this.toggleClass();
+    },
+    selectImageIdx(newIndex) {
+      this.currentImageIdx = newIndex;
+      this.toggleClass();
+    },
+
+    //  BOTTOM SLIDER
+    nextSlide() {
       if (this.imagesIndex >= this.imagesList.length - 1) {
         this.imagesIndex = 0;
       } else {
@@ -52,7 +96,7 @@ new Vue({
       }
     },
 
-    previousSlide: function () {
+    previousSlide() {
       if (this.imagesIndex < 1) {
         this.imagesIndex = this.imagesList.length - 1;
       } else {
@@ -60,12 +104,11 @@ new Vue({
       }
     },
 
-    clickedDot: function (index) {
+    clickedDot(index) {
       this.imagesIndex = index;
     },
 
-    // NAVBAR scroll effect
-
+    // NAVBAR COLLAPSE EFFECT
     scrollDetect(home, down, up) {
       // Current scroll position
       const currentScroll = this.scrollTop();
@@ -90,35 +133,15 @@ new Vue({
       this.navBar.collapse = true;
       this.navBar.open = false;
     },
-    // Called when scolled up
+    // Called when scrolled up
     scrollUp() {
       this.navBar.collapse = false;
       this.navBar.open = true;
     }, 
-
-    // main background slider
-    next: function() {
-      this.currentImageIdx += 1;
-      if(this.currentImageIdx > this.images.length - 1) {
-        this.currentImageIdx = 0
-      }  
-    },
-    prev: function() {
-      this.currentImageIdx -= 1;
-      if(this.currentImageIdx < 0) {
-        this.currentImageIdx = this.images.length - 1  // ultima posizione dell'array
-      }
-    },
-    selectImageIdx: function(newIndex) {
-      this.currentImageIdx = newIndex;
-    }
-    
-    
-
   },
   mounted() {
-    // this.classChange();
-    // this.loopToggle();
+    
+
   },
 
   created() {
